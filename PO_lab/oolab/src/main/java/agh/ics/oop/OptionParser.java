71c7a1ecdc4e960b045f114a4bc6542145b595c7
jviiiia <147636370+jviiiia@users.noreply.github.com>
2023-11-14 18:52:50 +0100

    Update OptionParser.java

--- PO_lab/oolab/src/main/java/agh/ics/oop/OptionParser.java
+++ PO_lab/oolab/src/main/java/agh/ics/oop/OptionParser.java
@@ -1,24 +1,55 @@
 package agh.ics.oop;
-
 import agh.ics.oop.model.MoveDirection;
 
-public class OptionParser {
-    public static MoveDirection[] parse(String[] args) {
-        MoveDirection[] directions = new MoveDirection[args.length];
+// lab2
+//public class OptionParser {
+//   public static MoveDirection[] parse(String[] args) {
+//        MoveDirection[] directions = new MoveDirection[args.length];
+//
+//        for (int i = 0; i < args.length; i++) {
+//            String arg = args[i];
+//            directions[i] = switch (arg) {
+//                case "f" -> MoveDirection.FORWARD;
+//                case "b" -> MoveDirection.BACKWARD;
+//                case "r" -> MoveDirection.RIGHT;
+//                case "l" -> MoveDirection.LEFT;
+//                default -> throw new IllegalArgumentException(arg + "is not legal move specification");
+//            };
+//        }
+//
+//        return directions;
+//    }
+//}
 
-        for (int i = 0; i < args.length; i++) {
-            String arg = args[i];
-            directions[i] = switch (arg) {
+//lab4
+import agh.ics.oop.model.MoveDirection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OptionParser {
+    public static List<MoveDirection> parse(String[] args) {
+        List<MoveDirection> directions = new ArrayList<>();
+
+        for (String arg : args) {
+            MoveDirection direction = switch (arg) {
                 case "f" -> MoveDirection.FORWARD;
                 case "b" -> MoveDirection.BACKWARD;
                 case "r" -> MoveDirection.RIGHT;
                 case "l" -> MoveDirection.LEFT;
-                default -> throw new IllegalArgumentException(arg + "is not legal move specification");
+                default -> null;
             };
+
+            if (direction != null) {
+                directions.add(direction);
+            }
         }
 
         return directions;
     }
 }
+
+
+
 
 
