ebb53f650ebfd2a20164a81776fdd9e70c94df40
jviiiia <147636370+jviiiia@users.noreply.github.com>
2023-11-21 16:47:11 +0100

    Update Simulation.java

--- PO_lab/oolab/src/main/java/agh/ics/oop/Simulation.java
+++ PO_lab/oolab/src/main/java/agh/ics/oop/Simulation.java
@@ -1,26 +1,34 @@
 package agh.ics.oop;
+
 import agh.ics.oop.model.Animal;
 import agh.ics.oop.model.Vector2d;
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.WorldMap;
+
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Simulation {
-    private List<Animal> animals;
-    private List<MoveDirection> directions;
+    private final WorldMap map;
+    private final List<Animal> animals;
+    private final List<MoveDirection> directions;
 
-    public Simulation(List<Vector2d> initialPositions, List<MoveDirection> directions) {
+    public Simulation(WorldMap map, List<Vector2d> startingPositions, List<MoveDirection> directions) {
+        this.map = map;
         this.animals = new ArrayList<>();
-        for (Vector2d position : initialPositions) {
-            this.animals.add(new Animal(position));
+        for (Vector2d position : startingPositions) {
+            Animal animal = new Animal(position);
+            this.animals.add(animal);
+            map.place(animal);
         }
-        // inicjowanie listy ruchów
+
         this.directions = directions;
     }
+
     public List<Animal> getAnimals() {
         return animals;
     }
+
     public void run() {
         System.out.println(animals);
 
@@ -30,11 +38,12 @@ public class Simulation {
         int i = 0;
         for (MoveDirection currentMove : directions) {
             Animal currentAnimal = animals.get(i % numberOfAnimals);
-            currentAnimal.move(currentMove);
-            System.out.println("Animal " + i % numberOfAnimals + ": " + currentAnimal.toString());
+            currentAnimal.move(currentMove, map);
+            System.out.println("Animal " + i % numberOfAnimals + ": " + currentAnimal);
             i++;
         }
     }
+}
 
 
 
