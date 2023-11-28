9a15649d7151d0ce5def364d5dd67dc723c8878b
jviiiia <147636370+jviiiia@users.noreply.github.com>
2023-11-28 15:48:54 +0100

    Update Animal.java

--- PO_lab/oolab/src/main/java/agh/ics/oop/model/Animal.java
+++ PO_lab/oolab/src/main/java/agh/ics/oop/model/Animal.java
@@ -1,69 +1,61 @@
 package agh.ics.oop.model;
 
-public class Animal {
-    private MapDirection orientation;
+
+public class Animal implements WorldElement {
     private Vector2d position;
+    private MapDirection orientation;
+    private final Vector2d lowerLeft = new Vector2d(0,0);
+    private final Vector2d upperRight = new Vector2d(9,9);
 
-    public Animal() {
-        this.position = new Vector2d(2, 2);
-        this.orientation = MapDirection.NORTH;
-
-    }
-
-    public Animal(Vector2d startingPosition) {
-        this.position = startingPosition;
+    public Animal(){
+        this.position = new Vector2d(2,2);
         this.orientation = MapDirection.NORTH;
     }
-
-    @Override
-    public String toString() {
-        return switch (orientation) {
-            case NORTH -> "^";
-            case SOUTH -> "v";
-            case EAST -> ">";
-            case WEST -> "<";
-            default -> "?";
-        };
+    public Animal( Vector2d initialPosition){
+        this.position = initialPosition;
+        this.orientation = MapDirection.NORTH;
     }
 
-    public boolean isAt(Vector2d position) {
-        return this.position.equals(position);
+    public String toString(){
+        switch (orientation) {
+            case NORTH:
+                return "^";
+            case SOUTH:
+                return "v";
+            case WEST:
+                return "<";
+            case EAST:
+                return ">";
+            default:
+                return "?";
+        }
     }
-
-    public Vector2d getPosition() {
-
+    public boolean isAt(Vector2d targetPosition){
+        return this.position.equals(targetPosition);
+    }
+    public Vector2d getPosition(){
         return position;
     }
-
-    public MapDirection getOrientation() {
+    public MapDirection getOrientation(){
         return orientation;
     }
 
     public void move(MoveDirection direction, MoveValidator validator) {
         switch (direction) {
-            case RIGHT -> orientation = orientation.next();
             case LEFT -> orientation = orientation.previous();
-            case FORWARD -> moveForward();
-            case BACKWARD -> moveBackward();
-            default -> System.out.println("Unknown direction.");
+            case RIGHT -> orientation = orientation.next();
+            case FORWARD -> {
+                Vector2d newPosition = position.add(orientation.toUnitVector());
+                if (validator.canMoveTo(newPosition)) {
+                    position = newPosition;
+                }
+            }
+            case BACKWARD -> {
+                Vector2d newPosition = position.subtract(orientation.toUnitVector());
+                if (validator.canMoveTo(newPosition)) {
+                    position = newPosition;
+                }
+            }
         }
-    }
-
-    private void moveForward() {
-        Vector2d newPosition = position.add(orientation.toUnitVector());
-        if (isInsideMap(newPosition)) {
-            position = newPosition;
-        }
-    }
-
-    private void moveBackward() {
-        Vector2d newPosition = position.subtract(orientation.toUnitVector());
-        if (isInsideMap(newPosition)) {
-            position = newPosition;
-        }
-    }
-
-    private boolean isInsideMap(Vector2d newPosition) {
-        return newPosition.follows(new Vector2d(0, 0)) && newPosition.precedes(new Vector2d(4, 4));
     }
 }
