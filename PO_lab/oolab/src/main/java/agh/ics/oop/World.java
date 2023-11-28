ee4ad3db7b2933269a2a08315b10b9444465f82d
jviiiia <147636370+jviiiia@users.noreply.github.com>
2023-11-28 15:47:01 +0100

    Update World.java

--- PO_lab/oolab/src/main/java/agh/ics/oop/World.java
+++ PO_lab/oolab/src/main/java/agh/ics/oop/World.java
@@ -2,34 +2,38 @@ package agh.ics.oop;
 import agh.ics.oop.model.*;
 import agh.ics.oop.model.util.*;
 
+
 import java.util.List;
 
 public class World {
     public static void main (String[] args){
-
-        // Wyświetlamy całą mapę
-        //System.out.println(visualizer.draw(new Vector2d(0, 0), new Vector2d(9, 9)));
-        WorldMap map = new RectangularMap(10,10);
+        WorldMap map = new GrassField(15);
         MapVisualizer visualizer = new MapVisualizer(map);
         Animal animal1 = new Animal(new Vector2d(2,4));
         Animal animal2 = new Animal(new Vector2d(5,7));
 
 
-        animal1.move(MoveDirection.BACKWARD, map);
-        animal2.move(MoveDirection.BACKWARD, map);
-        animal2.move(MoveDirection.LEFT, map);
-        animal2.move(MoveDirection.FORWARD, map);
-        animal2.move(MoveDirection.FORWARD, map);
+        map.place(animal1);
+        map.place(animal2);
+
+        System.out.println(visualizer.draw(new Vector2d(0, 0), new Vector2d(map.getWidth(), map.getHeight())));
 
 
+        map.move(animal1,MoveDirection.BACKWARD );
+        map.move(animal1,MoveDirection.BACKWARD);
+        map.move(animal2,MoveDirection.LEFT);
+        map.move(animal2,MoveDirection.FORWARD);
+        map.move(animal2,MoveDirection.FORWARD);
 
         map.place(animal1);
         map.place(animal2);
 
-        System.out.println("Movement status:");
-        System.out.println(visualizer.draw(new Vector2d(0, 0), new Vector2d(9, 9)));
+
+        System.out.println("Stan gry po ruchu:");
+        System.out.println(visualizer.draw(new Vector2d(0, 0), new Vector2d(map.getWidth(), map.getHeight())));
+
 
     };
-}
 
+}
 
