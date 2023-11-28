d45802b4df0269e3c2ca2e20f75c3cd86bac599a
jviiiia <147636370+jviiiia@users.noreply.github.com>
2023-11-28 16:55:08 +0100

    Update MoveDirection.java

--- PO_lab/oolab/src/main/java/agh/ics/oop/model/MoveDirection.java
+++ PO_lab/oolab/src/main/java/agh/ics/oop/model/MoveDirection.java
@@ -5,4 +5,19 @@ public enum MoveDirection {
     BACKWARD,
     RIGHT,
     LEFT;
+
+    public Vector2d toUnitVector() {
+        switch (this) {
+            case FORWARD:
+                return new Vector2d(0, 1);
+            case BACKWARD:
+                return new Vector2d(0, -1);
+            case RIGHT:
+                return new Vector2d(1, 0);
+            case LEFT:
+                return new Vector2d(-1, 0);
+            default:
+                throw new IllegalArgumentException("Unknown direction");
+        }
+    }
 }
